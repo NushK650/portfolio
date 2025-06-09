3adcc4a96f3a564aad92174342b8b5c3195ac6ca
NushK650 <Nush8653@gmail.com>
2025-06-09 15:48:37 -0700

    fixing the itinera link

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -8,7 +8,7 @@ function Projects() {
         <h2 className="text-3xl font-semibold text-gray-800 mb-8 text-center">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
-          <a href="https://itineraio.vercel.app/">
+          <a href="https://itineraio-git-main-karens-projects-ca6e76a2.vercel.app/">
           <div className="bg-gray-100 rounded-lg shadow-md p-6 hover:scale-105 hover:border-2 hover:border-blue-400">
             <p className=' font-bold text-center text-slate-500'>Itinera.io</p>
             <img className='h-full aspect-video mt-2' src="./Itinera.io.png" alt="Itinera.io" />
