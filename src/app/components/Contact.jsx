f26b18881d4954e0796a1a10db2e15208828b4dc
NushK650 <Nush8653@gmail.com>
2025-10-23 11:31:15 -0700

    fixes 10/23 i changes the bottom icons into something sleeker

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Contact = () => {
+  const ContactIcons = [
+    
+  ]
   return (
     <div  className="pt-20 pb-20 dark:bg-neutral-900 bg-gray-50 h-full">
     <div className="container mx-auto px-6 text-center">
@@ -11,20 +14,20 @@ const Contact = () => {
         Feel free to reach out! You can contact me via  <br /> Email: TanushKumar650@gmail.com
        <br /> Number: 650-218-4892 
       </p>
-      <div className="flex justify-center gap-10 ">
-        <div className='bg-gray-50 rounded-full p-2 '>
+      <div className="flex justify-center">
+        <div>
         <a href="https://github.com/NushK650">
-          <img src="./github.png" alt="github" />
+          <img src="./Icons/github.svg" alt="github" />
         </a>
         </div>
-        <div className='bg-gray-50 rounded-full p-2'>
+        <div>
         <a href="https://www.linkedin.com/in/tanush-kumar-aab68135b/details/skills/">
-          <img src="./linkedin.png"  alt="linkedin" />
+          <img src="./Icons/linkedin.svg"  alt="linkedin" />
         </a>
         </div>
-        <div className='bg-gray-50 rounded-full p-2 '>
+        <div>
         <a href="./ResumeV4.pdf">
-          <img src="./resume.png" alt="resume" />
+          <img src="./Icons/file-user.svg" alt="resume" />
         </a>
         </div>
       </div>
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Header() {
   return (
     
-    <div className="bg-white dark:bg-neutral-800 dark:text-white shadow py-4 px-6 fixed top-0 left-0 right-0 z-50  items-center flex justify-between text-slate-500">
+    <div className="bg-white dark:bg-neutral-800 dark:text-white shadow py-4 px-6 fixed top-0 left-0 right-0 z-50 bg- items-center flex justify-between text-slate-500">
       <div className="font-bold text-xl">
         <p>Tanush R. Kumar</p>
       </div>
@@ -12,9 +12,9 @@ function Header() {
       <nav>
         <ul className="flex space-x-4 ">
           
-          <li><a href="#about" className="hover:text-blue-600 text-lg font-medium hover:transition-all hover:text-xl  hover:duration-500">About</a></li>
-          <li><a href="#project" className="hover:text-blue-600 text-lg hover:transition-all hover:text-xl hover:duration-300 ">Projects</a></li>
-          <li><a href="#contact" className="hover:text-blue-600 text-lg hover:transition-all hover:text-xl hover:duration-300">Contact</a></li>
+          <li><a href="#about" className="hover:text-blue-600 text-lg font-medium hover:transition-all  hover:duration-500">About</a></li>
+          <li><a href="#project" className="hover:text-blue-600 text-lg hover:transition-all  hover:duration-300 ">Projects</a></li>
+          <li><a href="#contact" className="hover:text-blue-600 text-lg hover:transition-all  hover:duration-300">Contact</a></li>
         </ul>
       </nav>
     </div>
